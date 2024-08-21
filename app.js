b8c5db0447305c93817fe311d6dff101e4492e52
bipins867 <bipins867@gmail.com>
2024-08-21 12:24:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express=require('express')
+const cors=require('cors')
+const path=require('path')
+const bodyParser=require('body-parser')
+
+
+app=express()
+
+app.use(cors({
+    origin:'*',
+    methods:['GET','POST']
+}))
+app.use(bodyParser.json({extends:false}))
+
+
+
+
+
+
+
+app.listen(process.env.APP_PORT)
