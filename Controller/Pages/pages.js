519a9ba635df758b9db6f4c169f217071a7dc530
bipins867 <bipins867@gmail.com>
2024-09-02 14:06:08 +0530

    privacyPolicyPage Integrated

--- Controller/Pages/pages.js
+++ Controller/Pages/pages.js
@@ -11,3 +11,7 @@ exports.getHomePage=async(req,res,next)=>{
 exports.getCarrerPage=async(req,res,next)=>{
     res.sendFile(path.resolve('views','career.html'))
 }
+
+exports.getPrivacyPolicyPage=async(req,res,next)=>{
+    res.sendFile(path.resolve('views','privacyPolicy.html'))
+}
--- Routes/Pages/getRouter.js
+++ Routes/Pages/getRouter.js
@@ -5,6 +5,7 @@ const pageController=require('../../Controller/Pages/pages')
 const router=express.Router();
 
 router.get('/career',pageController.getCarrerPage)
+router.get('/privacyPolicy',pageController.getPrivacyPolicyPage)
 router.get('/',pageController.getHomePage)
 
 
