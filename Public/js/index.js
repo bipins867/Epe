0479eaf00e8a1f605f9e478b3274b8af3e576dd1
bipins867 <bipins867@gmail.com>
2024-09-17 11:38:29 +0530

    updated the animation....

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", async function () {
     
     onLoadAnimation('hero','slide-in-right')
-    onLoadAnimation('features','slide-in-left')
+    
+    onLoadAnimation('feature-header','slide-in-down')
+    onLoadAnimation('feature-cards','slide-in-up')
+
     onLoadAnimation('about-slide-container-left','slide-in-right')
     onLoadAnimation('about-slide-container-right','slide-in-left')
     onLoadAnimation('about-main-header','slide-in-up')
 
+
+    onLoadAnimation('services-item','fade-in')
     
     
     
