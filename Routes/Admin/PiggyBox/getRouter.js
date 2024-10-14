d5bf5cf3d8afc59618dbe40acd30d2b8ae950f10
bipins867 <bipins867@gmail.com>
2024-10-14 16:53:20 +0530

    Updated the href of the different different new pages...

--- Routes/Admin/PiggyBox/getRouter.js
+++ Routes/Admin/PiggyBox/getRouter.js
@@ -1,5 +1,10 @@
 const express = require("express");
 
+const piggyBoxPageController=require('../../../Controller/Pages/Admin/PiggyBox/PiggyBox/pages')
+
 const router = express.Router();
+
+router.get('/manageFunds',piggyBoxPageController.getManageFundsPage)
+router.get('/',piggyBoxPageController.getDashboardPage)
 
 module.exports = router;
