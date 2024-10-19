c19f7d6e4984286ee1bcdad47292b63f0b8ce26f
bipins867 <bipins867@gmail.com>
2024-10-19 16:45:57 +0530

    updated all the roles properlly...

--- Routes/Admin/Basic/basic.js
+++ Routes/Admin/Basic/basic.js
@@ -3,7 +3,8 @@ const getRouter=require('./getRouter')
 const postRouter=require('./postRouter');
 const applyLoanRouter=require('./ApplyLoan/applyLoan')
 const contactUsRouter=require('./ContactUs/contactUs')
-const announcementRouter=require('./Announcement/announcement')
+const announcementRouter=require('./Announcement/announcement');
+const { adminAuthentication } = require('../../../Middleware/auth');
 
 
 const router=express.Router();
@@ -11,7 +12,7 @@ const router=express.Router();
 router.use('/announcement',announcementRouter)
 router.use('/applyLoan',applyLoanRouter)
 router.use('/contactUs',contactUsRouter)
-router.use('/post',postRouter)
+router.use('/post',adminAuthentication,postRouter)
 router.use('/',getRouter)
 
 module.exports=router;
--- Routes/Admin/Basic/postRouter.js
+++ Routes/Admin/Basic/postRouter.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const router=express.Router();
 
-const basicController=require('../../../Controller/Admin/Basic/basic')
+const basicController=require('../../../Controller/Admin/Basic/basic');
+const { contactUsRole, applyLoanRole } = require('../../../Middleware/role');
+const { roleAuthentication } = require('../../../Middleware/auth');
 
 
-router.post('/contactUs',basicController.updateContactUsByAdmin)
-router.post('/applyLoan',basicController.updateApplyLoanByAdmin)
+router.post('/contactUs',contactUsRole,roleAuthentication,basicController.updateContactUsByAdmin)
+router.post('/applyLoan',applyLoanRole,roleAuthentication,basicController.updateApplyLoanByAdmin)
 
 module.exports=router;
