0e3ff93a17cb38df4567648044cac625dd410faa
bipins867 <bipins867@gmail.com>
2024-11-13 19:02:11 +0530

    updated the referrall information...

--- Controller/Users/PiggyBox/referral.js
+++ Controller/Users/PiggyBox/referral.js
@@ -53,7 +53,7 @@ exports.getReferalInfo = async (req, res, next) => {
     let baseUrl;
 
     if (process.env.NODE_ENV === "testing") {
-      baseUrl = `http://localhost:3000/`;
+      baseUrl = `${process.env.HOSTNAME}3000/`;
     } else {
       baseUrl = `https://epeindia.in/`;
     }
